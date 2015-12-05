b9ddfc3ba226557dda9ede83ba860ad2ff1d9c2e
Dusan Klinec <dusan.klinec@gmail.com>
2015-12-05 15:48:59 +0100

    build for coverity fixed as well

--- build-travis.sh
+++ build-travis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase cachedb_mongodb" all
+make exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase cachedb_mongodb mi_xmlrpc" all
